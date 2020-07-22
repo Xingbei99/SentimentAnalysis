967c8057937c82192a904c754a28b23a778b9ac2
Xingbei (Casey) <51894877+Xingbei99@users.noreply.github.com>
2020-07-22 07:44:27 -0500

    Documentation

--- src/main/scala/spark/Utils.scala
+++ src/main/scala/spark/Utils.scala
@@ -2,50 +2,78 @@ package spark
 
 import scala.io.{AnsiColor, Source}
 
-// Defines helper functions for SentimentAnalyzer
+/**
+ *  Defines helper functions for SentimentAnalyzer
+ */
 object Utils {
-  
+  /**
+   * Read the file from resources folder and convert it to a set.
+   * @param resourcePath: path of the file to read
+   * @return The file converted to a set
+   */
   def load(resourcePath: String): Set[String] = {
-    // Source.fromInputStream reads data from input string into Source object
-    // getClass.getResourceAsStream uses class loader to load the resource
-    val source = Source.fromInputStream(getClass.getResourceAsStream(resourcePath))
-    // Store words from source object into a set
-    val words = source.getLines.toSet
-    source.close()
+    val bufSource = Source.fromInputStream(getClass.getResourceAsStream(resourcePath))
+    val words = bufSource.getLines.toSet
+    bufSource.close() // Close the source while not using it
     words
   }
 
-  // Extract all words of the passed tweet text and store them in a list
+  /**
+   * Extract all words of the passed tweet text and store them in a list
+   * @param tweetText: tweet text
+   * @return
+   */
   def getWordList(tweetText: String): Seq[String] = {
       tweetText.split(" ")
   }
-  // Convert each word in wordList to lower case
+
+  /**
+   * Convert each word in wordList to lower case
+   * @param wordList: list of all words in the text of current tweet
+   * @return
+   */
   def toLower(wordList: Seq[String]): Seq[String] = {
-    // Seq type has map function that processes each element of the sequence
     wordList.map(_.toLowerCase)
   }
 
-  // Only keep words with only letters in a given word list
+  /**
+   * Only keep words with only letters in a given word list
+   * @param wordList: list of all words in the text of current tweet
+   * @return
+   */
   def filterNonWords(wordList: Seq[String]): Seq[String] = {
-    // Seq type has filter function that preserves the elements satisfying the passed condition
-    // matches() returns true if the string matches the regular expression
-    // + in regex: the string matches the previous expression of + more than once
     wordList.filter(_.matches("[a-z]+"))
   }
 
-  // Filters out all neutral words in a given word list and returns a new list
+  /**
+   * Filters out all neutral words in a given word list and returns a new list
+   * @param wordList: list of all words in the text of current tweet after preprocessing
+   * @param neutralWords: neutral words library
+   * @return
+   */
   def filterNeutralWords(wordList: Seq[String], neutralWords: Set[String]): Seq[String] = {
-    // filterNot returns a new list consisting all the elements of the list
-    // which does not satisfies the given predicate.
     wordList.filterNot(word => neutralWords.contains(word))
   }
 
-  // Calculate the score of a given tweet by adding up scores of its words
+  /**
+   *
+   * @param wordList: list of all words in the text of current tweet after preprocessing
+   * @param posWords: positive words library
+   * @param negWords: negative words library
+   * @return The sentiment score of the current tweet
+   */
   def getTweetScore(wordList: Seq[String], posWords: Set[String], negWords: Set[String]): Int = {
-    wordList.map(word => getWordScore(word, posWords, negWords)).sum
+    wordList.map(word => getWordScore(word, posWords, negWords))
+            .sum
   }
 
-  // Get the score of a given word
+  /**
+   * Get the score of a given word
+   * @param word: input word
+   * @param posWords
+   * @param negWords
+   * @return The sentiment score of the given word
+   */
   def getWordScore(word: String, posWords: Set[String], negWords: Set[String]): Int = {
     if (posWords.contains(word)) 1
     else if (negWords.contains(word)) -1
